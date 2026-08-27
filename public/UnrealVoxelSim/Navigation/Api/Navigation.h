ea2cce4ad2ba899268f934324297ea96c5c32d8e
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-08-27 18:41:29 +0200

    Remove proxy header

--- public/UnrealVoxelSim/Navigation/Api/Navigation.h
+++
@@ -1,9 +0,0 @@
-// TODO remove proxy header
-
-#pragma once
-#include "UnrealVoxelSim/Navigation/Api/ICommandProcessor.h"
-#include "UnrealVoxelSim/Navigation/Api/ICommandSink.h"
-#include "UnrealVoxelSim/Navigation/Api/IExecutionReader.h"
-#include "UnrealVoxelSim/Navigation/Api/IFollowingUpdater.h"
-#include "UnrealVoxelSim/Navigation/Api/IPlanner.h"
-#include "UnrealVoxelSim/Navigation/Api/IReachability.h"
